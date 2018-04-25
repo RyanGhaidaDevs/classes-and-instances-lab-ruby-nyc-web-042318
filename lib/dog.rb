d8757ab3b7bcb08491f6637d7dd7d49b4ca90fc7
Ryan Ghaida <ryaboug@gmail.com>
2018-04-25 06:35:17 -0400

    Done

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+    #code describing dog
+end
+
+fido = Dog.new
+
+snoopy = Dog.new
+
+lassie = Dog.new
+
+
+
+
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+
+class Person
+    #code describing person
+end
+
+adele_goldberg = Person.new
+
+alan_kay = Person.new 
